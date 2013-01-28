80822b2dcb720e11ed6b0578244f185f9418e57a
Antoine Turmel <geekshadow@gmail.com>
2013-01-28 16:47:06 +0100

    absolute url for notification image

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function playradio() {
 
     if (navigator.mozNotification) {
       document.body.classList.add("notification");
-      notification = navigator.mozNotification.createNotification("RTL2 Player", "Lecture en cours", "images/favicon.png");
+      notification = navigator.mozNotification.createNotification("RTL2 Player", "Lecture en cours", "http://geekshadow.github.com/rtl2player/images/favicon.png");
     }
     notification.show();
 }
